a41b08ad153645cebfa66df499a25b9ba9ebd050
zeeshan <229724567+zeeshanahmed001za-cmd@users.noreply.github.com>
2026-03-11 16:23:15 +0530

    add footer structure and design

--- javaScript/landingPage.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.querySelector(".navbar");
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
-});
